c2e13d0f8631c3a062b3010974bff85326a7a5b0
Alphen95 <57586762+Alphen95@users.noreply.github.com>
2021-02-08 21:23:45 +0500

    Обновление 2 - большой коммит
    -исправлены кавычки
    -убраны глобальные переменные/константы
    -константы вынесены за ifmain
    -и немного чистки в readme.md

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import urllib.parse
 from dotenv import load_dotenv
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("bitlink", help="ссылка на сайт/битлинк")
+args = parser.parse_args()
+load_dotenv()
+URL_INPUT = args.bitlink
+URL_CLICK_SUMMARY_TEMPLATE = "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary"
+BITLY_TOKEN = os.getenv("BITLY_TOKEN")
+
 
 def count_clicks(token, user_input):
     parsed_url = urllib.parse.urlparse(user_input)
     url_cut = "{}{}".format(parsed_url.netloc, parsed_url.path)
-    url = URL_CLICK_SUMMARY_TEMPLATE.format(url_cut)
+    url_full = URL_CLICK_SUMMARY_TEMPLATE.format(url_cut)
     header = {
         "Authorization": "Bearer {}".format(token)
     }
-    response = requests.get(url, headers=header)
+    response = requests.get(url_full, headers=header)
     if response.ok:
         usable_response = response.json()
         clicks_count = usable_response["total_clicks"]
@@ -23,11 +31,12 @@ def count_clicks(token, user_input):
 
 
 def shorten_link(token, url):
+    print("a")
     bitlink_data = {"long_url": url}
     header = {
         "Authorization": "Bearer {}".format(token)
     }
-    url_shorten = 'https://api-ssl.bitly.com/v4/shorten'
+    url_shorten = "https://api-ssl.bitly.com/v4/shorten"
     response = requests.post(url_shorten, headers=header, json=bitlink_data)
     if response.ok:
         usable_response = response.json()
@@ -40,28 +49,20 @@ def check_link(url, token):
     header = {
         "Authorization": "Bearer {}".format(token)
     }
-    url_check = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(url)
+    url_check = "https://api-ssl.bitly.com/v4/bitlinks/{}".format(url)
     response = requests.get(url_check, headers=header)
     return response.ok
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("bitlink", help="ссылка на сайт/битлинк")
-    arguments_all = parser.parse_args()
-    ARGS = arguments_all.bitlink
-    URL_CLICK_SUMMARY_TEMPLATE = "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary"
-    load_dotenv()
-    BITLY_TOKEN = os.getenv("BITLY_TOKEN")
-
-    if not check_link(ARGS, BITLY_TOKEN):
-        bitlink = shorten_link(BITLY_TOKEN, ARGS)
+    if not check_link(URL_INPUT, BITLY_TOKEN):
+        bitlink = shorten_link(BITLY_TOKEN, URL_INPUT)
         if bitlink == "error":
             print("Был введён неверный URL, попробуйте заново.")
         else:
             print("Битлинк:", bitlink)
     else:
-        clicks = count_clicks(BITLY_TOKEN, ARGS)
+        clicks = count_clicks(BITLY_TOKEN, URL_INPUT)
         if clicks == "error":
             print("Ошибка в программе, или в ссылке.")
         else:
